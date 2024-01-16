eeb9d963655a2b17ef333dfd531f2442e6b356fe
Saatwik Vasishtha <88363398+Teak-Rosewood@users.noreply.github.com>
2024-01-16 11:16:18 +0530

    Add files via upload

---
+++ week_1/8.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+
+int getOnesComplement(int num){
+	int i = 0;
+	int bin = 0;
+	while(num > 0){
+		int rem = num % 2;
+		if(rem == 0)
+  		    bin = bin + (pow(10, i));
+		i++;
+		num = num / 2;
+	}
+	return bin;
+}
+
+int binaryToDecimal(int bin){
+	int num = bin; 
+    int dec_value = 0; 
+    int base = 1; 
+    int temp = num; 
+    while (temp) { 
+        int last_digit = temp % 10; 
+        temp = temp / 10; 
+        dec_value += last_digit * base; 
+        base = base * 2; 
+    } 
+    return dec_value; 
+}
+
+int main(){
+	//printf("%d", getOnesComplement(10));
+	int m, n, i;
+	printf("Enter the dimentions of matrix: ");
+	scanf("%d", &m);
+	scanf("%d", &n);
+	int A[m][n];
+	int B[m][n];
+	int C[m][n];
+	for(i = 0; i < m; i++){
+		int j = 0;
+		for(j = 0; j < n; j++){
+			scanf("%d", &A[i][j]);
+			B[i][j] = A[i][j];
+			C[i][j] = B[i][j];
+		}
+	}
+	
+	omp_set_num_threads(m - 2);
+	#pragma omp parallel for
+	for(i = 1; i < m-1; i++){
+		int j = 1;
+		for(j = 1; j < n-1; j++){
+			printf("%d \n", A[omp_get_thread_num()+1][j]);
+			B[omp_get_thread_num()+1][j] = getOnesComplement(A[omp_get_thread_num()+1][j]);
+			C[omp_get_thread_num()+1][j] = binaryToDecimal(B[omp_get_thread_num()+1][j]);
+		}
+	}
+	int j = 0;
+	printf("Matrix A: \n");
+	for(i = 0; i < m; i++){
+		for(j = 0; j < n; j++){
+			printf("%d ", A[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("Matrix B: \n");
+	for(i = 0; i < m; i++){
+		for(j = 0; j < n; j++){
+			printf("%d ", B[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("Matrix C: \n");
+	for(i = 0; i < m; i++){
+		for(j = 0; j < n; j++){
+			printf("%d ", C[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+	
+}
